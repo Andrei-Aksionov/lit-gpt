700831ac8b74d95f9b958a5463791d49e5b9c146
Andrei-Aksionov <aksionau.andrei@gmail.com>
2023-11-21 17:03:14 +0300

    Update

--- lit_gpt/utils.py
+++ lit_gpt/utils.py
@@ -16,6 +16,7 @@ import torch
 import torch.nn as nn
 import torch.utils._device
 from lightning.fabric.strategies import FSDPStrategy
+
 from lightning.fabric.utilities.load import _lazy_load as lazy_load
 from torch.serialization import normalize_storage_type
 
