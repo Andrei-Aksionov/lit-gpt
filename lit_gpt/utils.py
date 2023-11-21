7aca032677207ac1b8e1c72f0bee01b41cdbf795
Andrei-Aksionov <aksionau.andrei@gmail.com>
2023-11-21 17:12:15 +0300

    Update

--- lit_gpt/utils.py
+++ lit_gpt/utils.py
@@ -15,6 +15,7 @@ import torch
 
 import torch.nn as nn
 import torch.utils._device
+
 from lightning.fabric.strategies import FSDPStrategy
 
 from lightning.fabric.utilities.load import _lazy_load as lazy_load
